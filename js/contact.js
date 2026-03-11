15b34c6730ef04c8899a3f8a3ee3b556cae03ad2
offground <wool7749@gmail.com>
2026-03-11 21:50:25 +0900

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,58 @@
+/* 연락처 자동 하이픈 */
+function formatPhone(input) {
+    var num = input.value.replace(/[^0-9]/g, '');
+
+    if (num.length <= 3) {
+        input.value = num;
+    } else if (num.substring(0, 2) === '02') {
+        // 서울 02
+        if (num.length <= 5) {
+            input.value = num.substring(0, 2) + '-' + num.substring(2);
+        } else if (num.length <= 9) {
+            input.value = num.substring(0, 2) + '-' + num.substring(2, 5) + '-' + num.substring(5);
+        } else {
+            input.value = num.substring(0, 2) + '-' + num.substring(2, 6) + '-' + num.substring(6, 10);
+        }
+    } else {
+        // 010, 031 등
+        if (num.length <= 7) {
+            input.value = num.substring(0, 3) + '-' + num.substring(3);
+        } else if (num.length <= 10) {
+            input.value = num.substring(0, 3) + '-' + num.substring(3, 6) + '-' + num.substring(6);
+        } else {
+            input.value = num.substring(0, 3) + '-' + num.substring(3, 7) + '-' + num.substring(7, 11);
+        }
+    }
+}
+
+/* 이메일 도메인 선택 */
+function selectEmailDomain(select) {
+    var domainInput = document.getElementById('emailDomain');
+    if (select.value === '') {
+        domainInput.value = '';
+        domainInput.readOnly = false;
+        domainInput.focus();
+    } else {
+        domainInput.value = select.value;
+        domainInput.readOnly = true;
+    }
+    updateEmailHidden();
+}
+
+function updateEmailHidden() {
+    var id = document.getElementById('emailId').value.trim();
+    var domain = document.getElementById('emailDomain').value.trim();
+    document.getElementById('email').value = (id && domain) ? id + '@' + domain : '';
+}
+
+/* 이메일 입력 시 hidden 필드 동기화 */
+document.addEventListener('DOMContentLoaded', function() {
+    var emailId = document.getElementById('emailId');
+    var emailDomain = document.getElementById('emailDomain');
+    if (emailId) emailId.addEventListener('input', updateEmailHidden);
+    if (emailDomain) emailDomain.addEventListener('input', updateEmailHidden);
+});
+
 // ===================================
 // ASTIK 문의 폼 - 구글 시트 연동
 // ===================================
