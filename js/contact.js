e4c1daf9e6277191765bdb465f2e8687d3dbf703
offground <wool7749@gmail.com>
2026-03-20 23:55:40 +0900

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -222,9 +222,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 fetch(form.action, {
                     method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
+                    mode: 'no-cors',
                     body: JSON.stringify(formData)
                 })
                 .then(function () {
